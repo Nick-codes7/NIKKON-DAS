3db318698129f82e30d9a84e53c536d09e6c5844
Nick-codes7 <164985864+Nick-codes7@users.noreply.github.com>
2024-04-21 15:16:14 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+df=pd.read_csv("C:\\Users\\NIKKON DAS\\OneDrive\\Desktop\\COVID 19 PROJECT'\\archive\\Latest Covid-19 India Status.csv")
+print(df)
+x=df['Deaths']
+y=df['Total Cases']
+plt.plot(x,y)
+plt.title("India Covid-19 Analysis",loc="left")
+plt.xlabel("Deaths")
+plt.ylabel("Total Cases")
+plt.show()
